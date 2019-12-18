d58554f802067bbd5f8cb98d535e93a6c9ad1719
pcf26536 <muffwaindan@gmail.com>
2019-12-18 17:03:37 +0300

    [ft #170307224] add core values desktop media query

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,10 +12,11 @@ const Card = (props) => {
           className={'card-image'}
           alt={props.alt}
       />
-      <div className={'shade'}/>
-      <div className={'value-title'}>{props.title}</div>
-      <hr className={'divider'}/>
-      <div className="value-desc">{props.text}</div>
+      <div className={'text'}>
+        <div className={'value-title'}>{props.title}</div>
+        <hr className={'divider'}/>
+        <div className="value-desc">{props.text}</div>
+      </div>
     </div>
   );
 };
