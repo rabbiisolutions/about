5be570de57905cf028a119275b35314e4f02b14c
pcf26536 <muffwaindan@gmail.com>
2020-02-17 12:26:39 +0300

    [ft #170307224] update menu links and fix packages

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -6,6 +6,8 @@ const telkom = '0772 915 361';
 const sema = 'sema@rabbii.co.ke';
 const menuTitle = 'Menu';
 const menuItems = ['Home', 'Payment', 'Become a Tutor', 'About Us', 'Trending', 'FAQs'];
+const menuLinks = ['https://rabbii.co.ke', 'https://payment.rabbii.co.ke', 'https://tutor.rabbii.co.ke',
+  'https://trending.rabbii.co.ke', 'https://faqs.rabbii.co.ke'];
 const ourServices = ['TeeCha'];
 const hidden = 'hidden';
 const slogan = 'We Believe Knowledge is Power';
@@ -13,7 +15,8 @@ const slogan = 'We Believe Knowledge is Power';
 
 const texts = {
   menuTitle: menuTitle, menuItems: menuItems, ourServices: ourServices, hidden: hidden, signUp: signUp,
-  contacts: contacts, findUs: findUs, safaricom: safaricom, telkom: telkom, sema: sema, slogan: slogan
+  contacts: contacts, findUs: findUs, safaricom: safaricom, telkom: telkom, sema: sema, slogan: slogan,
+  menuLinks: menuLinks
 };
 
 export default texts;
